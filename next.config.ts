8b439a2ac9eb82c49de4f7cf051cdbd0c592059e
Marek Elmayan <marek.elmayan@theodo.com>
2025-04-07 16:33:11 +0200

    clean: remove unused files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: "export", // doesn't work with server actions
 };
 
 export default nextConfig;

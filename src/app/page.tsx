fe4304c434b4bc4e9e2badce3fd6d06d059e4e0b
Marek Elmayan <marek.elmayan@theodo.com>
2025-04-07 11:27:44 +0200

    feat: add a bit of layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,18 @@ export default async function Home() {
   const trends = await getTrendsData();
 
   return (
-    <main>
-      <h1>Hello World</h1>
-      <pre>{JSON.stringify(trends, null, 2)}</pre>
+    <main className="p-4 bg-amber-50">
+      <h1 className="text-2xl font-bold text-center mb-4">
+        Technology Evolution Trends
+      </h1>
+      <div className="flex gap-2 bg-white p-4 rounded-lg">
+        <div className="flex flex-col gap-2 w-1/3">
+          <h2 className="text-lg font-bold">Technologies</h2>
+          <pre>{JSON.stringify(trends, null, 2)}</pre>
+        </div>
+        <div className="border-l-2 border-gray-200 h-auto w-1" />
+        <h2 className="text-lg font-bold">Graph</h2>
+      </div>
     </main>
   );
 }

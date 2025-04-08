6252746c24b7894a1abda138e5d28d153330cecb
Marek Elmayan <marek.elmayan@theodo.com>
2025-04-08 11:00:07 +0200

    refactor: layout

--- src/app/components/TechnoSelection.tsx
+++ src/app/components/TechnoSelection.tsx
@@ -2,7 +2,6 @@
 
 import { Dispatch, SetStateAction } from "react";
 import { Trend } from "../page";
-import LinearChart from "./LinearChart";
 import debounce from "lodash.debounce";
 import { CheckboxItem } from "./CheckboxItem";
 
@@ -30,71 +29,65 @@ export function TechnoSelection({
     );
   };
   return (
-    <div className="flex gap-2 bg-white p-4 rounded-lg">
-      <div className="flex flex-col gap-3 w-1/3">
-        <h2 className="text-lg font-bold">Technologies</h2>
-        <div className="flex gap-2">
-          <button
-            className={`bg-amber-500 text-amber-950 px-4 py-2 rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-auto`}
-            onClick={() =>
-              setTrends((prev) => prev.map((t) => ({ ...t, selected: true })))
-            }
-            disabled={trends.every((t) => t.selected)}
-          >
-            Select All
-          </button>
-          <button
-            className={`bg-amber-500 text-amber-950 px-4 py-2 rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-auto`}
-            onClick={() =>
-              setTrends((prev) => prev.map((t) => ({ ...t, selected: false })))
-            }
-            disabled={trends.every((t) => !t.selected)}
-          >
-            Deselect All
-          </button>
-        </div>
-        <div className="flex gap-2 items-center">
-          <label htmlFor="min-rank-change" className="text-lg font-medium">
-            Min rank change
-          </label>
-          <input
-            type="number"
-            id="min-rank-change"
-            placeholder="Search"
-            className="border-2 border-gray-300 rounded-md p-2"
-            onChange={debounce(handleMinRankChange, 600)}
-          />
-        </div>
-        <div className="grid grid-cols-2 gap-2">
-          {trends
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .filter((trend) => trend.selected)
-            .map((trend) => (
-              <CheckboxItem
-                key={trend.name}
-                label={trend.name}
-                checked={true}
-                onChange={() => handleChange(trend)}
-                color={trend.color}
-              />
-            ))}
-          {trends
-            .sort((a, b) => a.name.localeCompare(b.name))
-            .filter((trend) => !trend.selected)
-            .map((trend) => (
-              <CheckboxItem
-                key={trend.name}
-                label={trend.name}
-                checked={false}
-                onChange={() => handleChange(trend)}
-                color={trend.color}
-              />
-            ))}
-        </div>
+    <div className="flex flex-col gap-3 w-1/3">
+      <h2 className="text-lg font-bold">Technologies</h2>
+      <div className="flex gap-2">
+        <button
+          className={`bg-amber-500 text-amber-950 px-4 py-2 rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-auto`}
+          onClick={() =>
+            setTrends((prev) => prev.map((t) => ({ ...t, selected: true })))
+          }
+          disabled={trends.every((t) => t.selected)}
+        >
+          Select All
+        </button>
+        <button
+          className={`bg-amber-500 text-amber-950 px-4 py-2 rounded-md cursor-pointer disabled:opacity-50 disabled:cursor-auto`}
+          onClick={() =>
+            setTrends((prev) => prev.map((t) => ({ ...t, selected: false })))
+          }
+          disabled={trends.every((t) => !t.selected)}
+        >
+          Deselect All
+        </button>
       </div>
-      <div className="border-l-2 border-gray-200 h-auto w-1" />
-      <div className="flex gap-2 w-2/3">
-        <LinearChart trends={trends.filter((t) => t.selected)} />
+      <div className="flex gap-2 items-center">
+        <label htmlFor="min-rank-change" className="text-lg font-medium">
+          Min rank change
+        </label>
+        <input
+          type="number"
+          id="min-rank-change"
+          placeholder="Search"
+          className="border-2 border-gray-300 rounded-md p-2"
+          onChange={debounce(handleMinRankChange, 600)}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        {trends
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .filter((trend) => trend.selected)
+          .map((trend) => (
+            <CheckboxItem
+              key={trend.name}
+              label={trend.name}
+              checked={true}
+              onChange={() => handleChange(trend)}
+              color={trend.color}
+            />
+          ))}
+        {trends
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .filter((trend) => !trend.selected)
+          .map((trend) => (
+            <CheckboxItem
+              key={trend.name}
+              label={trend.name}
+              checked={false}
+              onChange={() => handleChange(trend)}
+              color={trend.color}
+            />
+          ))}
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import LinearChart from "./components/LinearChart";
 import { TechnoSelection } from "./components/TechnoSelection";
 import { fixRankByDate, stringToColor } from "./utils";
 
@@ -85,7 +86,13 @@ export default function Home() {
       <h1 className="text-2xl font-bold text-center mb-4">
         Technology Evolution Trends
       </h1>
-      <TechnoSelection trends={trends} setTrends={setTrends} />
+      <div className="flex gap-2 bg-white p-4 rounded-lg">
+        <TechnoSelection trends={trends} setTrends={setTrends} />
+        <div className="border-l-2 border-gray-200 h-auto w-1" />
+        <div className="flex gap-2 w-2/3">
+          <LinearChart trends={trends.filter((t) => t.selected)} />
+        </div>
+      </div>
     </main>
   );
 }
